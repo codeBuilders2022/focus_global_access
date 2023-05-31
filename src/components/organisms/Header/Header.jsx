e39ad5312f2168d580bc663666f09d40c664163c
codeBuilders2022 <codebuilders2022@gmail.com>
2023-05-30 23:18:17 -0600

    theme

--- src/components/organisms/Header/Header.jsx
+++ src/components/organisms/Header/Header.jsx
@@ -9,13 +9,18 @@ import search from "../../../assets/icons/search.svg"
 import at from "../../../assets/icons/at.svg"
 import events from "../../../assets/icons/events.svg"
 import info from "../../../assets/icons/info.svg"
+import Moon from '../../../assets/images/moon.png'
+import Sun from '../../../assets/images/sol.png'
 
 //react
 import { useNavigate, Link } from "react-router-dom"
+import { useState } from "react"
 
 
 
 const Header = () => {
+    const navigate = useNavigate()
+    const [theme, setTheme] = useState(false)
 
     const navBar = [
         { title: "Home", icon: home, url: "/" },
@@ -25,11 +30,17 @@ const Header = () => {
         { title: "Publish", icon: at,},
         { title: "About FOCUS G.A", icon: info,},
     ]
+
+    const handleTheme = () => {
+        document.body.classList.toggle("dark_")
+        setTheme(!theme)
+    }
+
+
   
-    const navigate = useNavigate()
 
     return (
-        <div className='Header'>
+        <header className='Header'>
             <div className="left">
                 <div className="logo">
                     <img className="logoImg" src={logo} onClick={() => navigate('/')} style={{ cursor: 'pointer' }} />
@@ -40,24 +51,26 @@ const Header = () => {
                         return (
                             <div className="optionsNav" key={index}>
                                 <Link to={element.url}>
-                                    <img className="icon" src={element.icon} />
+                                    <img className="icon_" src={element.icon} />
                                     {element.title}
                                 </Link>
-                                <div className="barButtom"></div>
                             </div>
                         )
                     })}
                 </nav>
             </div>
-            <div className="user">
+            <section className="user_t">
                 <button onClick={() => navigate('/register')}>Sign up</button>
                 <button onClick={() => navigate('/login')}>Log in</button>
-                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-moon" width="25" height="25" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
-                </svg>
-            </div>
-        </div>
+                <div className={`swith_ ${theme && "dark_"}`} onClick={handleTheme}>
+                    {theme ? (
+                        <img src={Moon} alt="" className="img_icon"/>
+                    ):(
+                        <img src={Sun} alt="" className="img_icon"/>
+                    )}
+                </div>
+            </section>
+        </header>
     )
 }
 
