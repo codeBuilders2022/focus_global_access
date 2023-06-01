80d55e7f5f58fd9eea1c56ece36c11e9e73455bf
jorgeAFD1 <jorge.appconecte@gmail.com>
2023-06-01 13:03:18 -0600

    skeletons

---
+++ src/Pages/Register/Step_2/Step_2Skeleton.jsx
@@ -0,0 +1,56 @@
+//styles
+import "./Step_2.scss"
+
+//assets
+import logo from "../../../assets/Logos/logo focus_w.png"
+import warning from "../../../assets/icons/warning.svg"
+
+//react
+import { useNavigate } from "react-router-dom"
+import { Dropdown } from "primereact/dropdown"
+import { InputText } from "primereact/inputtext"
+import { Button } from "primereact/button"
+import { Skeleton } from "primereact/skeleton"
+
+
+
+const Step_2Skeleton = () => {
+    return (
+        <div className="Step_2">
+            <div className="card">
+            </div>
+            <form className="Register">
+                <div className="containerR">
+                    <Skeleton width="150px" height="54px"></Skeleton>
+                    <div style={{ marginTop: "10px" }}>
+                        <Skeleton width="100px" height="30px"></Skeleton>
+                    </div>
+                    <div className="input">
+                        <Skeleton width="100px" height="20px"></Skeleton>
+                        <Skeleton width="100%" height="40px"></Skeleton>
+                    </div>
+                    <div className="input">
+                        <Skeleton width="100px" height="20px"></Skeleton>
+                        <Skeleton width="100%" height="40px"></Skeleton>
+                    </div>
+                    <div className="input">
+                        <Skeleton width="100px" height="20px"></Skeleton>
+                        <Skeleton width="100%" height="40px"></Skeleton>
+                    </div>
+                    <div style={{ display: 'flex', alignItems: 'center', textAlign: 'start', marginTop: '10px' }}>
+                    <Skeleton width="100%" height="42px"></Skeleton>
+                    </div>
+
+                    <div className="button" style={{ flexDirection: 'row', display: 'flex', gap: '20px' }}>
+
+                    <Skeleton width="100%" height="50px"></Skeleton>
+                    <Skeleton width="100%" height="50px"></Skeleton>
+
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Step_2Skeleton
--- src/components/organisms/Journal/Journal.jsx
+++ src/components/organisms/Journal/Journal.jsx
@@ -1,28 +1,55 @@
 import { Link, NavLink } from 'react-router-dom'
 import "./Journal.scss"
 import GeneralButton from '../../atoms/GeneralButton/GeneralButton'
+import { Skeleton } from 'primereact/skeleton'
 
-const Journal = ({ newURL, journal, index }) => {
+const Journal = ({ newURL, journal, index, skeleton }) => {
     return (
         <div className="journal" key={index}>
-            <Link>
-                <img src={journal.image} />
-            </Link>
-            <div className="info">
-                <h3>{journal.name}</h3>
-                <div className='data'>
-                    <p>Volume {journal.volume}</p>
-                    <p>|</p>
-                    <p>ISNN 1638-0915</p>
-                    <p>|</p>
-                    <Link to={"#"}>Journal website</Link>
-                </div>
-                <p>{journal.abstract}</p>
-                <p className='p_impact'>Impact: <span className="impact">{journal.impact}</span></p>
-                <Link>
-                    <GeneralButton/>
-                </Link>
-            </div>
+            {
+                !skeleton ?
+                    <>
+                        <Link>
+                            <img src={journal.image} />
+                        </Link>
+                        <div className="info">
+                            <h3>{journal.name}</h3>
+                            <div className='data'>
+                                <p>Volume {journal.volume}</p>
+                                <p>|</p>
+                                <p>ISNN 1638-0915</p>
+                                <p>|</p>
+                                <Link to={"#"}>Journal website</Link>
+                            </div>
+                            <p>{journal.abstract}</p>
+                            <p className='p_impact'>Impact: <span className="impact">{journal.impact}</span></p>
+                            <Link>
+                                <GeneralButton />
+                            </Link>
+                        </div>
+                    </> :
+                    <>
+                        <Skeleton height='350px'></Skeleton>
+                        <div className="info">
+                            <h3><Skeleton height='20px' width='100px'></Skeleton></h3>
+                            <div className='data'>
+                                <p><Skeleton height='20px' width='100px'></Skeleton></p>
+                                <p>|</p>
+                                <p><Skeleton height='20px' width='150px'></Skeleton></p>
+                                <p>|</p>
+                                <Link to={"#"}><Skeleton height='20px' width='100px'></Skeleton></Link>
+                            </div>
+                            <p><Skeleton height='120px'></Skeleton></p>
+                            <p className='p_impact' style={{ marginTop: "15px" }}><Skeleton height='30px' width='200px'></Skeleton></p>
+                            <Link>
+                                <div style={{ marginTop: "24px",width:"100%" }}>
+                                    <Skeleton height='40px' ></Skeleton>
+                                </div>
+                            </Link>
+                        </div>
+                    </>
+            }
+
         </div>
     )
 }
